129200b89a32c433cb6a46b5b0b9303131737279
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2026-06-30 14:37:47 +0200

    Remove MuxStrategyForMLP and mux/ directory
    Delete MuxStrategyForMLP and all supporting files:
    MuxDepositor, MuxProxy, MuxOrderHandler, and 10 interfaces.
    No external references to any file in the mux/ directory.

--- contracts/strategies/arbitrum/mux/interfaces/IVault.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-interface IVault {
-    function liquidityPool() external view returns (address);
-    function orderBook() external view returns (address);
-}
