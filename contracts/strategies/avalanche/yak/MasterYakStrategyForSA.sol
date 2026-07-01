7ee6416183c3547e5931d6610d02f6bbf7b4884f
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2026-07-01 15:53:01 +0200

    Refactor MasterYakStrategyForSA to use BaseStrategy
    Move MasterYakStrategyForSA from the legacy YakStrategy base onto
    BaseStrategy/YakStrategyV3. Implement the standard BaseStrategy hooks
    for staking, withdrawals, reward claiming, reward conversion, emergency
    withdraw, and totalDeposits.
    
    Remove now-orphaned YakStrategy and Permissioned contracts.
    
    Verified with forge build.

--- contracts/strategies/avalanche/yak/MasterYakStrategyForSA.sol
+++ contracts/strategies/avalanche/yak/MasterYakStrategyForSA.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "../../../YakStrategy.sol";
+import "../../BaseStrategy.sol";
 import "../../../interfaces/IPair.sol";
-import "../../../interfaces/IERC20.sol";
-import "../../../interfaces/IWAVAX.sol";
 import "../../../lib/DexLibrary.sol";
 
 import "./interfaces/IYakChef.sol";
@@ -13,170 +11,62 @@ import "./interfaces/IYakChef.sol";
  * @notice Strategy for Master Yak, which pays rewards in AVAX
  * @dev Fees are paid in WAVAX
  */
-contract MasterYakStrategyForSA is YakStrategy {
-
+contract MasterYakStrategyForSA is BaseStrategy {
     IYakChef public stakingContract;
     IPair private swapPairToken;
     uint256 public PID;
 
     constructor(
-        string memory _name,
-        address _depositToken,
-        address _rewardToken,
         address _stakingContract,
         address _swapPairToken,
         uint256 pid,
-        address _timelock,
+        BaseStrategySettings memory _baseStrategySettings,
         StrategySettings memory _strategySettings
-    ) YakStrategy(_strategySettings) {
-        name = _name;
-        depositToken = IERC20(_depositToken);
-        rewardToken = IERC20(_rewardToken);
+    ) BaseStrategy(_baseStrategySettings, _strategySettings) {
         stakingContract = IYakChef(_stakingContract);
         swapPairToken = IPair(_swapPairToken);
-        devAddr = msg.sender;
         PID = pid;
-
-        setAllowances();
-        updateDepositsEnabled(true);
-        transferOwnership(_timelock);
-
-        emit Reinvest(0, 0);
     }
 
     receive() external payable {}
 
-    function setAllowances() public onlyOwner {
-        depositToken.approve(address(stakingContract), MAX_UINT);
-    }
-
-    function deposit(uint256 amount) external override nonReentrant {
-        _deposit(msg.sender, amount);
-    }
-
-    function depositWithPermit(
-        uint256 amount,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external override nonReentrant {
-        depositToken.permit(msg.sender, address(this), amount, deadline, v, r, s);
-        _deposit(msg.sender, amount);
-    }
-
-    function depositFor(address account, uint256 amount) external override nonReentrant {
-        _deposit(account, amount);
-    }
-
-    function _deposit(address account, uint256 amount) private onlyAllowedDeposits {
-        require(DEPOSITS_ENABLED == true, "MasterYakStrategy::_deposit");
-        if (MAX_TOKENS_TO_DEPOSIT_WITHOUT_REINVEST > 0) {
-            uint256 unclaimedRewards = checkReward();
-            if (unclaimedRewards > MAX_TOKENS_TO_DEPOSIT_WITHOUT_REINVEST) {
-                _reinvest(unclaimedRewards);
-            }
-        }
-        require(depositToken.transferFrom(msg.sender, address(this), amount));
-        _stakeDepositTokens(amount);
-        _mint(account, getSharesForDepositTokens(amount));
-        totalDeposits = totalDeposits + amount;
-        emit Deposit(account, amount);
-    }
-
-    function withdraw(uint256 amount) external override nonReentrant {
-        uint256 depositTokenAmount = getDepositTokensForShares(amount);
-        if (depositTokenAmount > 0) {
-            _withdrawDepositTokens(depositTokenAmount);
-            _safeTransfer(address(depositToken), msg.sender, depositTokenAmount);
-            _burn(msg.sender, amount);
-            totalDeposits = totalDeposits - depositTokenAmount;
-            emit Withdraw(msg.sender, depositTokenAmount);
-        }
-    }
-
-    function _withdrawDepositTokens(uint256 amount) private {
-        require(amount > 0, "MasterYakStrategy::_withdrawDepositTokens");
-        stakingContract.withdraw(PID, amount);
-    }
-
-    function reinvest() external override onlyEOA nonReentrant {
-        uint256 unclaimedRewards = checkReward();
-        require(unclaimedRewards >= MIN_TOKENS_TO_REINVEST, "MasterYakStrategy::reinvest");
-        _reinvest(unclaimedRewards);
-    }
-
-    /**
-     * @notice Reinvest rewards from staking contract to deposit tokens
-     * @dev Reverts if the expected amount of tokens are not returned from `stakingContract`
-     */
-    function _reinvest(uint256 amount) private {
-        stakingContract.deposit(PID, 0);
-        IWAVAX(address(rewardToken)).deposit{value: amount}();
-
-        uint256 devFee = (amount * DEV_FEE_BIPS) / BIPS_DIVISOR;
-        if (devFee > 0) {
-            _safeTransfer(address(rewardToken), devAddr, devFee);
-        }
-
-        uint256 reinvestFee = (amount * REINVEST_REWARD_BIPS) / BIPS_DIVISOR;
-        if (reinvestFee > 0) {
-            _safeTransfer(address(rewardToken), msg.sender, reinvestFee);
-        }
-
-        uint256 depositTokenAmount = DexLibrary.swap(
-            amount - devFee - reinvestFee,
-            address(rewardToken),
-            address(depositToken),
-            swapPairToken
-        );
-
-        _stakeDepositTokens(depositTokenAmount);
-        totalDeposits = totalDeposits + depositTokenAmount;
-
-        emit Reinvest(totalDeposits, totalSupply);
-    }
-
-    function _stakeDepositTokens(uint256 amount) private {
-        require(amount > 0, "MasterYakStrategy::_stakeDepositTokens");
+    function _depositToStakingContract(uint256 _amount, uint256 _depositFee) internal override {
+        uint256 amount = _amount - _depositFee;
+        depositToken.approve(address(stakingContract), amount);
         stakingContract.deposit(PID, amount);
     }
 
-    /**
-     * @notice Safely transfer using an anonymous ERC20 token
-     * @dev Requires token to return true on transfer
-     * @param token address
-     * @param to recipient address
-     * @param value amount
-     */
-    function _safeTransfer(
-        address token,
-        address to,
-        uint256 value
-    ) private {
-        require(IERC20(token).transfer(to, value), "MasterYakStrategy::TRANSFER_FROM_FAILED");
+    function _withdrawFromStakingContract(uint256 _amount) internal override returns (uint256 withdrawAmount) {
+        stakingContract.withdraw(PID, _amount);
+        return _amount;
     }
 
-    function checkReward() public view override returns (uint256) {
-        uint256 pendingReward = stakingContract.pendingRewards(PID, address(this));
-        uint256 contractBalance = address(this).balance;
-        return pendingReward + contractBalance;
+    function _emergencyWithdraw() internal override {
+        stakingContract.emergencyWithdraw(PID);
+        depositToken.approve(address(stakingContract), 0);
     }
 
-    function estimateDeployedBalance() external view override returns (uint256) {
+    function _pendingRewards() internal view override returns (Reward[] memory) {
+        Reward[] memory pendingRewards = new Reward[](1);
+        pendingRewards[0] = Reward({reward: address(WGAS), amount: stakingContract.pendingRewards(PID, address(this))});
+        return pendingRewards;
+    }
+
+    function _getRewards() internal override {
+        stakingContract.deposit(PID, 0);
+        uint256 balance = address(this).balance;
+        if (balance > 0) {
+            WGAS.deposit{value: balance}();
+        }
+    }
+
+    function _convertRewardTokenToDepositToken(uint256 _fromAmount) internal override returns (uint256 toAmount) {
+        if (_fromAmount == 0) return 0;
+        return DexLibrary.swap(_fromAmount, address(rewardToken), address(depositToken), swapPairToken);
+    }
+
+    function totalDeposits() public view override returns (uint256) {
         (uint256 amount, ) = stakingContract.userInfo(PID, address(this));
         return amount;
-    }
-
-    function rescueDeployedFunds(uint256 minReturnAmountAccepted, bool disableDeposits) external override onlyOwner nonReentrant {
-        uint256 balanceBefore = depositToken.balanceOf(address(this));
-        stakingContract.emergencyWithdraw(PID);
-        uint256 balanceAfter = depositToken.balanceOf(address(this));
-        require(balanceAfter - balanceBefore >= minReturnAmountAccepted, "MasterYakStrategy::rescueDeployedFunds");
-        totalDeposits = balanceAfter;
-        emit Reinvest(totalDeposits, totalSupply);
-        if (DEPOSITS_ENABLED == true && disableDeposits == true) {
-            updateDepositsEnabled(false);
-        }
     }
 }
